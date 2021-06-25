dbbc71fcf61fa83773acd14f3c56cf8a1ef09b4a
Felix Sun <sunfmin@gmail.com>
2021-06-25 16:29:14 +0800

    session 1 with xuxin, try to add retry policy

--- admin/que.go
+++ admin/que.go
@@ -8,19 +8,87 @@ import (
 
 	"github.com/goplaid/web"
 	"github.com/goplaid/x/presets"
+	. "github.com/goplaid/x/vuetify"
 	"github.com/theplant/go-que-admin/config"
 	"github.com/theplant/go-que-admin/models"
 	h "github.com/theplant/htmlgo"
 	"github.com/tnclong/go-que"
 )
 
+func retryPolicyEditor(j *models.GoqueJob) h.HTMLComponent {
+	return VContainer(
+		VMenu(
+			web.Slot(
+				h.A().Text("Retry Policy").Attr("v-on:click", "vars.myMenuShow = true"),
+			).Name("activator"),
+
+			VCard(
+				VDivider(),
+				VList(
+					VListItem(
+						VListItemTitle(h.Text("Initial Interval")),
+						VListItemAction(
+							VTextField().
+								FieldName("RetryPolicy.InitialInterval").
+								Value(fmt.Sprint(int64(j.RetryPolicy.InitialInterval))),
+						),
+					),
+					VListItem(
+						VListItemAction(
+							VSwitch().Color("purple").
+								FieldName("EnableHints").
+								InputValue(true),
+						),
+						VListItemTitle(h.Text("Enable hints")),
+					),
+				),
+
+				VCardActions(
+					VSpacer(),
+					VBtn("Cancel").Text(true).
+						On("click", "vars.myMenuShow = false"),
+					VBtn("Save").Color("primary").
+						Text(true).OnClick("submit"),
+				),
+			),
+		).CloseOnContentClick(false).
+			NudgeWidth(200).
+			OffsetY(true).
+			Attr("v-model", "vars.myMenuShow"),
+	).Attr(web.InitContextVars, `{myMenuShow: false}`)
+}
+
 func configQue(b *presets.Builder) {
 	m := b.Model(&models.GoqueJob{})
+
+	d := m.Detailing("ID", "Queue", "Actions")
+
+	d.Field("Queue").ComponentFunc(func(obj interface{}, field *presets.FieldContext, ctx *web.EventContext) h.HTMLComponent {
+
+		j := obj.(*models.GoqueJob)
+		return VCard(
+			VCardTitle(h.Text(j.Queue)),
+			VCardText(
+				VChip(h.Text("5:30")),
+				VChip(h.Text("6:30")),
+			),
+		)
+
+	})
+
+	d.Field("Actions").ComponentFunc(func(obj interface{}, field *presets.FieldContext, ctx *web.EventContext) h.HTMLComponent {
+
+		//j := obj.(*models.GoqueJob)
+		return VBtn("Back").Href("/admin/goque-jobs")
+	})
+
+	_ = d
+
 	l := m.Listing("ID", "Queue", "Args", "RunAt", "DoneAt", "RetryCount", "LastErrMsg", "UniqueID", "UniqueLifeCycle")
 
 	l.Field("RunAt").ComponentFunc(func(obj interface{}, field *presets.FieldContext, ctx *web.EventContext) h.HTMLComponent {
 		runAt := obj.(*models.GoqueJob).RunAt
-		return h.Td(h.Text(fmt.Sprint(runAt)))
+		return h.Td(h.Text(fmt.Sprint(runAt))).Style("width: 100px")
 	})
 
 	eb := m.Editing("Queue", "Args", "RetryPolicy")
@@ -30,19 +98,24 @@ func configQue(b *presets.Builder) {
 		return h.Div().Text(j.Queue)
 	})
 
+	eb.Field("RetryPolicy").ComponentFunc(func(obj interface{}, field *presets.FieldContext, ctx *web.EventContext) h.HTMLComponent {
+		j := obj.(*models.GoqueJob)
+		return retryPolicyEditor(j)
+	})
+
 	eb.SaveFunc(func(job interface{}, id string, ctx *web.EventContext) (err error) {
 		q := config.TheQ
 
 		j := job.(*models.GoqueJob)
+		fmt.Println("j.RetryPolicy.InitialInterval = ", j.RetryPolicy.InitialInterval)
 		var args []interface{}
 		json.Unmarshal([]byte(j.Args), &args)
-		var retryPolicy que.RetryPolicy
-		json.Unmarshal([]byte(j.RetryPolicy), &retryPolicy)
+		//json.Unmarshal([]byte(j.RetryPolicy), &retryPolicy)
 		_, err = q.Enqueue(context.Background(), nil, que.Plan{
 			Queue:       "import_pdf",
 			Args:        que.Args(args...),
 			RunAt:       time.Now(),
-			RetryPolicy: retryPolicy,
+			RetryPolicy: j.RetryPolicy.RetryPolicy,
 		})
 		return
 	})
--- models/job.go
+++ models/job.go
@@ -1,12 +1,18 @@
 package models
 
-import "time"
+import (
+	"database/sql/driver"
+	"encoding/json"
+	"errors"
+	queque "github.com/tnclong/go-que"
+	"time"
+)
 
 type GoqueJob struct {
 	ID              uint
 	Queue           string
 	Args            string
-	RetryPolicy     string
+	RetryPolicy     RetryPolicy
 	RunAt           time.Time
 	DoneAt          *time.Time
 	RetryCount      int
@@ -17,4 +23,25 @@ type GoqueJob struct {
 	UniqueLifeCycle int
 	UpdatedAt       time.Time
 	CreatedAt       time.Time
+}
+
+type RetryPolicy struct {
+	queque.RetryPolicy
+}
+
+func (p RetryPolicy) Value() (driver.Value, error) {
+	return json.Marshal(p)
+}
+
+func (p *RetryPolicy) Scan(data interface{}) error {
+	var byteData []byte
+	switch values := data.(type) {
+	case []byte:
+		byteData = values
+	case string:
+		byteData = []byte(values)
+	default:
+		return errors.New("scan DayParts unsupported type of data")
+	}
+	return json.Unmarshal(byteData, p)
 }
